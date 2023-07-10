95f4d56ce70c3c9367cbfa06f22740541ad5b9be
ahmadfarisi1 <ahmadfarisi12345@gmail.com>
2023-07-10 14:32:44 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Category from './src/Category'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Category />
+    );
+  }
+}
